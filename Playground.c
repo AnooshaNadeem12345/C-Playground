074af79c7ad3cbfd09ba75c68be013da84029b82
AnooshaNadeem12345 <anooshanadeem226@gmail.com>
2026-07-22 03:05:50 +0500

    Calculator Playground

---
+++ Playground.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    if (op == '+')
+        printf("Result: %.2lf\n", num1 + num2);
+    else if (op == '-')
+        printf("Result: %.2lf\n", num1 - num2);
+    else if (op == '*')
+        printf("Result: %.2lf\n", num1 * num2);
+    else if (op == '/')
+        printf("Result: %.2lf\n", num1 / num2);
+    else
+        printf("Invalid operator\n");
+
+    return 0;
+}
